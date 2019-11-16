ce55df28f08960f6d66e0e0a0f5af090838c40d1
Andreluizsgf <Andreluizsgf@gmail.com>
2019-11-16 20:18:39 -0300

    corrige

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -92,7 +92,7 @@
         Como posso mudar seu mundo hoje?
       </div>
       <!-- Default to the left -->
-      <strong>2019-{{\Carbon\Carbon::now()->year}} <a href="//github.com/lemosluan">Luan Lemos</a></strong>
+      <strong>2019-{{\Carbon\Carbon::now()->year}} <a href="https://github.com/andreluizsgf">André Luiz Gomes</a></strong>
     </footer>
   </div>
   <!-- ./wrapper -->
